e9850cb70102a38346cf427ab36bfa880696053a
bouboumael <mael-chariault_student2021@wilder.school>
2021-03-28 01:38:10 +0100

    finish

---
+++ Car.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'Vehicle.php';
+
+class Car extends Vehicle
+{
+
+    public function __construct(string $color, int $nbSeats, string $energy)
+    {
+        parent::__construct($color, $nbSeats);
+        $this->setEnergy($energy);
+    }
+
+}
